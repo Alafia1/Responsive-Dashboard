2559af457d391ae77181f380e901c0b8031f0f9f
Alafia1 <101114171+Alafia1@users.noreply.github.com>
2022-04-20 09:44:14 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [{
+    productName: 'Microwave 7Ghz',
+    productNumber: '82394',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+  {
+    productName: 'Microwave 18Ghz',
+    productNumber: '98932',
+    paymentStatus: 'Refunded',
+    shipping: 'Declined'
+  },
+  {
+    productName: 'Microwave 13Ghz',
+    productNumber: '738276',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Microwave 13Ghz',
+    productNumber: '823d94',
+    paymentStatus: 'Due',
+    shipping: 'Pending'
+  },
+
+]
